bd9a9ac75ede6dac9e2c41dc7e9cd4861011d1fd
Rana Ali Raza <alirazaue88@gmail.com>
2023-09-06 12:36:12 +0500

    added the  edit functionality to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,29 +1,47 @@
 import React, { useState } from "react";
-import { getAuth, signOut  } from 'firebase/auth';
+import { getAuth, signOut, updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import {doc, updateDoc} from 'firebase/firestore';
+import {db} from '../firebase'; 
 
 function Profile() {
   const auth = getAuth();
-  const navigate = useNavigate()
-  const [formData, setFormData] = useState({
-    name: auth.currentUser.displayName,
-    email: auth.currentUser.email,
-  });
-  const { name, email } = formData;
+  const navigate = useNavigate();
+  const [name, setName] = useState(auth.currentUser.displayName);
+  const [email, setEmail] = useState(auth.currentUser.email);
+  const [changeDetail, setChangeDetail] = useState(false); //for edit functionality part
+  
 
-  async function onLogout(){
+  async function onLogout() {
+    try {
+      await signOut(auth);
+      navigate("/");
+      toast.success("Sign out successful");
+    } catch (error) {
+      toast.error("Error logging out");
+    }
+  }
+  async function onSubmit(){
       try {
+        if(auth.currentUser.displayName !== name){
+          //update display name in firebase auth
+          await updateProfile(auth.currentUser, {
+            displayName: name,
+          });
 
-        await signOut(auth);
-        navigate("/");
-        toast.success("Sign out successful")
-        
+          //update name in firestore
+            const docRef = doc(db, "users", auth.currentUser.uid);
+            await updateDoc(docRef, {
+              name,
+            })
+
+        }
+        toast.success("Profile details updated");
       } catch (error) {
-        toast.error("Error logging out")
+        toast.error("Couldn't update the profile details")
       }
   }
-
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -34,23 +52,38 @@ function Profile() {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+              disabled={!changeDetail}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ${changeDetail  && "bg-red-200 focus:bg-red-200" }`}
             />
             <input
               type="email"
               id="email"
               value={email}
-              disabled
+              // onChange={(e) => setEmail(e.target.value)}
+              disabled={!changeDetail}
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
 
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center ">
                 Do you want to change your name?
-                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
+                </span>
               </p>
-              <p className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer" onClick={onLogout}>Sign out</p>
+              <p
+                className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer"
+                onClick={onLogout}
+              >
+                Sign out
+              </p>
             </div>
           </form>
         </div>
