300e5719b8514beb59b6e336b5d410228daf8daa
Rana Ali Raza <alirazaue88@gmail.com>
2023-09-06 16:26:26 +0500

    Created sspinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ useEffect(()=>{
         setPageState("Sign in")
       }
   })
-},[])
+},[auth])
 
   function pathMatchRoute(route) {
     const isMatch = route === location.pathname;
@@ -27,7 +27,7 @@ useEffect(()=>{
   }
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
